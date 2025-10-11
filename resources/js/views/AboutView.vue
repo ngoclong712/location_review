a4da3afd5e7fce47b34e16eac92a4a7204b3bc61
Ngọc Long <143398861+ngoclong712@users.noreply.github.com>
2025-10-11 21:47:43 +0700

    Test VueJS

---
+++ resources/js/views/AboutView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="p-6 text-center">
+        <h1 class="text-3xl font-bold text-green-600">ℹ️ About Page</h1>
+        <router-link
+            to="/"
+            class="text-green-500 hover:underline"
+        >
+            Quay lại Home
+        </router-link>
+    </div>
+</template>
---
+++ resources/js/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="p-6 text-center">
+        <h1 class="text-3xl font-bold text-blue-600">🏠 Home Page</h1>
+        <RouterLink
+            to="/about"
+            class="text-blue-500 hover:underline"
+        >
+            Đi đến About
+        </RouterLink>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
