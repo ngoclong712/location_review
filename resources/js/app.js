9999b0efe9a6e865f63c7b6a9ba93a9aecaf8076
Ngọc Long <143398861+ngoclong712@users.noreply.github.com>
2025-10-14 23:47:23 +0700

    config
    pinia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,17 @@
 import { createApp } from 'vue'
+import { createPinia } from "pinia";
 import App from './App.vue'
 import router from './router/index.js'
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 import './bootstrap';
 
-import '../css/app.css'
+import '../css/app.css';
+
+import "./router/guard";
 
 const app = createApp(App)
+const pinia = createPinia()
 app.use(Toast)
+app.use(pinia)
 app.use(router).mount('#app')
